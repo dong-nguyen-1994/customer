98f569342a4c5b85395f410c7a85a7e5562b6092
Đồng Nguyễn <dongbuh@gmail.com>
2020-09-25 01:34:07 +0700

    update icon

--- src/CustomerServiceProvider.php
+++ src/CustomerServiceProvider.php
@@ -108,7 +108,7 @@ class CustomerServiceProvider extends BaseModuleServiceProvider
     {
         Event::listen(CoreAdminMenuRegistered::class, function ($menu) {
 
-            $menu->add('Customer', ['id' => 'customer'])->data('order', 2000);
+            $menu->add('Customer', ['id' => 'customer'])->data('order', 2000)->prepend('<i class="fas fa-users"></i>');
             $menu->add('Customer', ['route' => 'customer.admin.customer.index', 'parent' => $menu->customer->id]);
             $menu->add('Customer Group', ['route' => 'customer.admin.group.index', 'parent' => $menu->customer->id]);
 
